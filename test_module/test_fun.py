c985735c179c624c065351c007d3dbdbedfe048b
Mateusz Łącki <matteo.lacki@gmail.com>
2017-08-11 19:36:34 +0200

    hahahaha

---
+++ test_module/test_fun.py
@@ -0,0 +1,27 @@
+def test_fun(x):
+	"""A test function
+
+	Parameters
+	----------
+		x : int
+			An int.
+	Returns
+	-------
+		x : int
+			The same fucking int.
+	Notes
+	-----
+	This is soo stupid.
+
+	Warning
+	-------
+		Don't do it at home!!!
+
+	Example
+	-------
+		>>> test_fun(10)
+		10
+	"""
+	print('This is x:', x)
+	return xls
+	
