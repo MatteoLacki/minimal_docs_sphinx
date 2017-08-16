479d1fdcf2e8e966876443b007a1aba2d0d60c50
Mateusz Łącki <matteo.lacki@gmail.com>
2017-08-16 13:11:04 +0200

    Changed the theme.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,11 +30,11 @@ sys.path.insert(0, os.path.abspath('../../'))
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc',
-    'sphinx.ext.doctest',
-    'sphinx.ext.mathjax',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.napoleon']
+extensions = [ 'sphinx.ext.autodoc',
+                'sphinx.ext.doctest',
+                'sphinx.ext.mathjax',
+                'sphinx.ext.viewcode',
+                'sphinx.ext.napoleon'  ]
 
 from recommonmark.parser import CommonMarkParser
 
@@ -93,7 +93,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
